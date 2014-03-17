024a95c3690b1c5e9040583542d8b2f6c198e152
tawaasalage <tawaasalage@gmail.com>
2014-03-17 21:08:24 +0530

    Interface variables initialized at the MainActivity.java file

--- src/com/project/Team/MainActivity.java
+++ src/com/project/Team/MainActivity.java
@@ -3,20 +3,27 @@ package com.project.Team;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	Button btnMAaddaNote;
+	EditText etAMtxtShopType;
+	EditText etAMtxtItemName;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		btnMAaddaNote=(Button)findViewById(R.id.btnMAaddaNote);
+		etAMtxtItemName=(EditText)findViewById(R.id.etAMtxtItemName);
+		etAMtxtShopType=(EditText)findViewById(R.id.etAMtxtShopType);
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 
 }
